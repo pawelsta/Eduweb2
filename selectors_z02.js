512f067e45b1b0ac6a07b278616b9f46071f4fed
Pawel Staniszewski <paul-85@o2.pl>
2017-03-09 02:11:40 +0100

    Pierwszy Commit

---
+++ selectors_z02.js
@@ -0,0 +1,18 @@
+(function($) {
+
+$(document).ready(function() {
+
+var pierwszy = $(".grid div, .grid-12 div");
+
+var drugi = $(".nav a[href^='http']");
+
+var trzeci = $(":radio:disabled, :checkbox:disabled");
+
+var czwarty = $(".div p:first:has(text['#'])");
+
+var piaty = $(".pagination-item:not('span')");
+
+
+});
+		
+})(jQuery);
